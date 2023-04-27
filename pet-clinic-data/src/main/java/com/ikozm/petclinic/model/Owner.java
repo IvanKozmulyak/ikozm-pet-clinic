3d037308ec66008e8e067d3d6ee0b39b4cc5b068
ikozm <ivankozmulyak@gmail.com>
2023-04-28 01:20:51 +0300

    Create Visit entity. Closes #25.

--- pet-clinic-data/src/main/java/com/ikozm/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/ikozm/petclinic/model/Owner.java
@@ -1,4 +1,14 @@
 package com.ikozm.petclinic.model;
 
-public class Owner extends Person{
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+public class Owner extends Person {
+
+    private Set<Pet> pets = new HashSet<>();
 }
