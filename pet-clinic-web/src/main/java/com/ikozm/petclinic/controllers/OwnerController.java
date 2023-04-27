381e184b2fcd0475e4d40047c86ba0b2ceb1111d
ikozm <ivankozmulyak@gmail.com>
2023-04-27 14:33:58 +0300

    Add list of owners to index page. Closes #15.

--- pet-clinic-web/src/main/java/com/ikozm/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/ikozm/petclinic/controllers/OwnerController.java
@@ -1,14 +1,27 @@
 package com.ikozm.petclinic.controllers;
 
+import com.ikozm.petclinic.model.Owner;
+import com.ikozm.petclinic.services.OwnerService;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Set;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listOwners(){
+    public String listOwners(Model model){
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
