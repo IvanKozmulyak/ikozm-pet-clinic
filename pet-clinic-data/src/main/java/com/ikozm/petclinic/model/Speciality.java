2143b44161ea033be3124b1b040679919a58006e
ikozm <ivankozmulyak@gmail.com>
2023-04-28 01:24:30 +0300

    Create Speciality entity. Closes #26.

---
+++ pet-clinic-data/src/main/java/com/ikozm/petclinic/model/Speciality.java
@@ -0,0 +1,11 @@
+package com.ikozm.petclinic.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class Speciality extends BaseEntity {
+
+    private String description;
+}
